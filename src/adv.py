52ead58844da348372817a36762aa022626d2a62
Rob Hamilton <RobHamilton3rd@gmail.com>
2020-02-11 19:22:28 +0100

    Tuesday Work

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,13 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
-room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+room = {'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+        'foyer':    Room("Foyer", """Dim light filters in from the south. Dust, passages run north and east."""),
+        'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling,into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
+        'narrow':   Room("Narrow Passage", """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
+        'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
 }
 
 
@@ -38,6 +27,11 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+user_name = input("What is your name? ")
+
+#user starts outside
+location = "outside"
+user = Player(room[location],user_name)
 
 # Write a loop that:
 #
@@ -49,3 +43,42 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+game_on = 1
+prompt = "Where do you wish to go? Valid Inputs: N W E S (type 'q' to quit)"
+intro = True
+
+while game_on:
+    if intro:
+        print("Welcome to Rob Quest!")
+        intro = False
+    print("You are in: ", user.location.name)
+    print("You see: ", user.location.description)
+    path = input(prompt)
+    if path == "q":
+        game_on = False
+    if path == "N":
+        if hasattr(user.location, 'n_to'):
+            user.location = user.location.n_to
+        else:
+            print("You can't go that way!")
+    if path == "S":
+        if hasattr(user.location, 's_to'):
+            user.location = user.location.s_to
+        else:
+            print("You can't go that way!")
+    if path == "E":
+        import pdb; pdb.set_trace()
+        if hasattr(user.location, 'e_to'):
+            user.location = user.location.e_to
+        else:
+            print("You can't go that way!")
+    if path == "W":
+        if hasattr(user.location, 'w_to'):
+            user.location = user.location.w_to
+        else:
+            print("You can't go that way!")
+    else:
+        print("That is not a direction!")
+    
+
--- src/player.py
+++ src/player.py
@@ -1,2 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self,location,name):
+        self.location = location
+        self.name = name
+    def __str__(self):
+        return f"{self.name} is in {self.location}"
